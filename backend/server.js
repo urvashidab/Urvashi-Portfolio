b456b6273e1a6a98a98ac2eb7ddaae1d4e32d91c
urvashidab <urvashi.dabgotra11@gmail.com>
2026-03-30 22:10:00 +0530

    mongoose added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
 dotenv.config();
 //middlewares
 app.use(cors());
+
+//parse json data
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
 // contact route
 app.route("api/contact");
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
 
 // initialize server
 app.listen(PORT, () => {
