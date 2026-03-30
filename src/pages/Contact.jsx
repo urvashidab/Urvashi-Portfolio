9da60b8532a983f990eb606af3773e292ce0799a
urvashidab <urvashi.dabgotra11@gmail.com>
2026-03-30 15:34:03 +0530

    contact page layout adjusted

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,122 +6,123 @@ import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
 const Contact = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 py-12">
-      {/* left side */}
+    <>
+      <h2 className="heading-section py-10">CONTACT</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 py-12">
+        {/* left side */}
 
-      <div className="flex flex-col gap-8">
-        <h2 className="heading-section">CONTACT</h2>
+        <div className="flex flex-col gap-8">
+          <p className=" max-w-md text-gray-600 dark:text-gray-400">
+            Let’s create something meaningful together. I’d love to hear about
+            your project, big or small.
+          </p>
 
-        <p className=" max-w-md text-gray-600 dark:text-gray-400">
-          Let’s create something meaningful together. I’d love to hear about
-          your project, big or small.
-        </p>
+          {/* contact info */}
 
-        {/* contact info */}
+          <div>
+            <h3 className="text-md tracking-[0.2em] uppercase  mb-4">
+              EMAIL & PHONE
+            </h3>
 
-        <div>
-          <h3 className="text-md tracking-[0.2em] uppercase  mb-4">
-            EMAIL & PHONE
-          </h3>
+            <div className="flex items-center gap-3 ">
+              <FaPhoneAlt className="text-sm" />
+              <span>(647) 965-0612</span>
+            </div>
 
-          <div className="flex items-center gap-3 ">
-            <FaPhoneAlt className="text-sm" />
-            <span>(647) 965-0612</span>
+            <div className="flex items-center gap-3  mt-4">
+              <MdEmail />
+              <a
+                href="mailto:urvashi.ucoe@gmail.com"
+                className="underline hover:text-gray-500 dark:hover:text-[#f1e1b9] transition"
+              >
+                urvashi.ucoe@gmail.com
+              </a>
+            </div>
           </div>
 
-          <div className="flex items-center gap-3  mt-4">
-            <MdEmail />
+          {/* social icons */}
+
+          <div className=" flex gap-8 mt-6 text-xl ">
+            {/* github */}
             <a
-              href="mailto:urvashi.ucoe@gmail.com"
-              className="underline hover:text-gray-500 dark:hover:text-[#f1e1b9] transition"
+              href="https://github.com/urvashidab"
+              target="_blank"
+              rel="noreferrer"
             >
-              urvashi.ucoe@gmail.com
+              <FaGithub className="social-btns" />
+            </a>
+            {/* insta */}
+            <a
+              href="https://github.com/urvashidab"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram className="social-btns" />
+            </a>
+            {/* twitter */}
+            <a
+              href="https://github.com/urvashidab"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaXTwitter className="social-btns" />
+            </a>
+
+            {/* facebook */}
+            <a
+              href="https://github.com/urvashidab"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookF className="social-btns" />
             </a>
           </div>
+
+          <p className="uppercase tracking-[0.2em] text-gray-500">
+            I’m currently accepting projects. Also, compliments.
+          </p>
         </div>
 
-        {/* social icons */}
+        {/* right side */}
 
-        <div className=" flex gap-8 mt-6 text-xl ">
-          {/* github */}
-          <a
-            href="https://github.com/urvashidab"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaGithub className="social-btns" />
-          </a>
-          {/* insta */}
-          <a
-            href="https://github.com/urvashidab"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaInstagram className="social-btns" />
-          </a>
-          {/* twitter */}
-          <a
-            href="https://github.com/urvashidab"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaXTwitter className="social-btns" />
-          </a>
+        <form className="flex flex-col gap-6">
+          {/* name */}
+          <input
+            type="text"
+            required
+            placeholder="NAME"
+            className="input-form "
+          />
 
-          {/* facebook */}
-          <a
-            href="https://github.com/urvashidab"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaFacebookF className="social-btns" />
-          </a>
-        </div>
+          {/* email */}
 
-        <p className="uppercase tracking-[0.2em] text-gray-500">
-          I’m currently accepting projects. Also, compliments.
-        </p>
+          <input
+            type="email"
+            required
+            placeholder="E-MAIL"
+            className="input-form "
+          />
+
+          {/* message */}
+
+          <textarea
+            required
+            rows="4"
+            placeholder="YOUR MESSAGE"
+            className="input-form resize-none"
+          ></textarea>
+
+          {/* submit button */}
+
+          <button
+            type="submit"
+            className="w-fit border px-10 py-3 mt-4 tracking-widest text-sm cursor-pointer hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black transition duration-200"
+          >
+            SUBMIT
+          </button>
+        </form>
       </div>
-
-      {/* right side */}
-
-      <form className="flex flex-col gap-6">
-        {/* name */}
-        <input
-          type="text"
-          required
-          placeholder="NAME"
-          className="input-form "
-        />
-
-        {/* email */}
-
-        <input
-          type="email"
-          required
-          placeholder="E-MAIL"
-          className="input-form "
-        />
-
-        {/* message */}
-
-        <textarea
-          required
-          rows="4"
-          placeholder="YOUR MESSAGE"
-          className="input-form resize-none"
-        ></textarea>
-
-        {/* submit button */}
-
-        <button
-          type="submit"
-          className="w-fit border px-10 py-3 mt-4 tracking-widest text-sm cursor-pointer hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black transition duration-200"
-        >
-          SUBMIT
-        </button>
-      </form>
-    </div>
+    </>
   );
 };
 
