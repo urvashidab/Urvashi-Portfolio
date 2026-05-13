7dbcef217c18bdce33e3be213072c94c915df12e
urvashidab <urvashi.dabgotra11@gmail.com>
2026-05-13 21:45:26 +0530

    cors url added

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ const app = express();
 connectDB();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(helmet());
 
